ff58854f7d316d5da41b4b36ead6b6ed785ed821
jacek-oet <jacek.bendig@openenergytransition.org>
2024-08-26 12:59:37 +0200

    Benchmark lp files (#8)

--- website/app.py
+++ website/app.py
@@ -1,5 +1,38 @@
 import streamlit as st
 
+st.markdown(
+  """
+  <style>
+    .banner-container {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      padding: 10px 15px;
+      text-align: center;
+      border-radius: 4px;
+      height: 56px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 18px;
+    }
+    
+    .appview-container {
+      margin-top: 56px;
+    }
+    
+    header.st-emotion-cache-12fmjuu {
+      top: 56px;
+    }
+  </style>
+  <div class="banner-container">
+      <strong>This website is under development. All content is for testing purposes, and is subject to change.</strong>
+  </div>
+  """,
+  unsafe_allow_html=True
+)
+
 pages = [
   st.Page("raw-results.py", title="Raw Results"),
   st.Page("compare.py", title="Compare solvers"),
