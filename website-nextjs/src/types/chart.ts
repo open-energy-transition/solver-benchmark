22e4d48384a9fde89afcdf4a19b096438c7cc6b3
jacek-oet <jacek.bendig@openenergytransition.org>
2025-06-23 08:21:59 +0200

    Website: Key Insights: factors affecting performance (#267)
    closes #255 
    
    
    ![image](https://github.com/user-attachments/assets/7734c11e-b284-4260-a226-2356ee93c987)
    
    ![image](https://github.com/user-attachments/assets/b905fe65-9222-4f05-a788-b52a98ce9023)
    
    ![image](https://github.com/user-attachments/assets/6eec5489-921d-4bde-9420-6322387419ba)
    
    ![image](https://github.com/user-attachments/assets/4bf2f536-6440-4efc-9be7-19164236ca6a)
    
    ![image](https://github.com/user-attachments/assets/e701b697-f4ef-4b44-8239-a2f7056d0980)
    
    ![image](https://github.com/user-attachments/assets/e387597d-a042-40f1-ac57-35fc5768a08e)
    
    ![image](https://github.com/user-attachments/assets/a5d78381-b944-49d1-8afd-fef441b599c1)
    
    ![image](https://github.com/user-attachments/assets/bc3b5ff6-d288-474e-bed2-4eb4dd66f32b)

--- website-nextjs/src/types/chart.ts
+++ website-nextjs/src/types/chart.ts
@@ -16,7 +16,8 @@ export interface ID3GroupedBarChart {
   chartData: StackedBarData[];
   categoryKey: string;
   barTextClassName?: (d: ID3GroupedBarChartData) => string;
-  xAxisTooltipFormat?: (d: ID3GroupedBarChartData) => string;
+  xAxisTooltipFormat?: (category: string) => string;
+  tooltipFormat?: (d: ID3GroupedBarChartData) => string;
   xAxisLabel?: string;
   yAxisLabel?: string;
   rotateXAxisLabels?: boolean;
@@ -24,6 +25,7 @@ export interface ID3GroupedBarChart {
   axisLabelTitle?: (d: ID3GroupedBarChartData) => string;
   barOpacity?: number | ((d: ID3GroupedBarChartData) => number);
   xAxisTickFormat?: (value: string, data: unknown) => string;
+  normalize?: boolean;
   colors:
     | {
         [key: string]: string;
@@ -31,6 +33,10 @@ export interface ID3GroupedBarChart {
     | ((d: ID3GroupedBarChartData) => string);
   transformHeightValue?: (d: ID3GroupedBarChartData) => number;
   xAxisBarTextClassName?: string;
+  customLegend?: (props: {
+    chartData: StackedBarData[];
+    categoryKey: string;
+  }) => React.ReactNode;
 }
 
 export interface ID3StackedBarChart {
