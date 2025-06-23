f95fe2aaac3646897a7b3b36711b0672da00a3df
jacek-oet <jacek.bendig@openenergytransition.org>
2025-06-23 19:05:53 +0200

    Add Umami script

--- website-nextjs/src/pages/_document.tsx
+++ website-nextjs/src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="d81ca88f-bdf3-486d-8de9-cf89d02744e2"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="antialiased">
