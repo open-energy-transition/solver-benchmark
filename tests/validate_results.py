fe3cc4c3d8dd025e03c4db43de420d9675e7c9aa
jacek-oet <jacek.bendig@openenergytransition.org>
2025-09-24 21:12:02 +0200

    Improve warning message format for unknown sizes in validation script

--- tests/validate_results.py
+++ tests/validate_results.py
@@ -17,7 +17,9 @@ for n, b in meta["benchmarks"].items():
         elif s["Size"] in ["S", "M"]:
             short_TO_benchs.add(n + "-" + s["Name"])
         else:
-            print(f"::warning Unknown size {s['Size']}")
+            print(
+                f"::warning file=tests/validate_results.py::ERROR: Unknown size {s['Size']}"
+            )
             raise ValueError(f"Unknown size {s['Size']}")
 
 data["bench-size"] = data["Benchmark"] + "-" + data["Size"]
@@ -35,7 +37,7 @@ for (bench, size), group in data.groupby(["Benchmark", "Size"]):
         else:
             if short_TO_solvers != str(solvers_present):
                 print(
-                    f"::warning Unexpected solvers for {bench_size}: {solvers_present}"
+                    f"::warning file=tests/validate_results.py::ERROR: Unexpected solvers for {bench_size}: {solvers_present}"
                 )
                 raise ValueError(
                     f"Unexpected solvers for {bench_size}: {solvers_present}"
@@ -47,13 +49,15 @@ for (bench, size), group in data.groupby(["Benchmark", "Size"]):
         else:
             if long_TO_solvers != str(solvers_present):
                 print(
-                    f"::warning Unexpected solvers for {bench_size}: {solvers_present}"
+                    f"::warning file=tests/validate_results.py::ERROR: Unexpected solvers for {bench_size}: {solvers_present}"
                 )
                 raise ValueError(
                     f"Unexpected solvers for {bench_size}: {solvers_present}"
                 )
     else:
-        print(f"::warning Unknown benchmark {bench_size}")
+        print(
+            f"::warning file=tests/validate_results.py::ERROR: Unknown benchmark {bench_size}"
+        )
         raise ValueError(f"Unknown benchmark {bench_size}")
     seen_benchs.add(bench_size)
 
@@ -62,6 +66,11 @@ print(f"Solvers run on long TO benchmarks:\n{long_TO_solvers}")
 
 # Check that no bench-size from metadata is missing
 missing_benchs = (short_TO_benchs | long_TO_benchs) - seen_benchs
+
 if missing_benchs:
-    print("::warning ERROR: couldn't find these benchs in the results:")
-    print(missing_benchs)
+    print(
+        "::warning file=tests/validate_results.py::ERROR: couldn't find these benchs in the results:"
+    )
+    print(
+        f"::warning file=tests/validate_results.py::ERROR: Missing benchmarks: {missing_benchs}"
+    )
