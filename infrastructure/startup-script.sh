f9bb8372c6f4d1adcef3f20d4ea97ff2c89c176d
Kristijan Faust <kristijan.faust@openenergytransition.org>
2025-09-25 08:51:09 +0200

    Checkout to the hipo branch implementation on machine boot

--- infrastructure/startup-script.sh
+++ infrastructure/startup-script.sh
@@ -20,7 +20,7 @@ gsutil cp gs://solver-benchmarks-restricted/gurobi-benchmark-40-session.lic /opt
 
 # Clone the repository
 echo "Cloning repository..."
-git clone --depth=1 -b run-v1 https://github.com/open-energy-transition/solver-benchmark.git
+git clone --depth=1 -b highs-hipo-runs https://github.com/open-energy-transition/solver-benchmark.git
 
 # Install a global highs binary for reference runs
 echo "Installing Highs..."
