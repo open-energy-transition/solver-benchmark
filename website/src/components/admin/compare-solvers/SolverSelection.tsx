eaa4d5a7bfbb8325acc1cbd0f126940f0077e38b
Jacek <jacek.bendig@openenergytransition.org>
2026-06-03 23:23:45 +0700

    Website: fix eslint issues (#516)
    **For changes to the website:**
    - [x] I have tested my changes by running the website locally
    
    <img width="714" height="192" alt="image"
    src="https://github.com/user-attachments/assets/ab9dc8d6-8a26-4bbe-98f2-f496a5a764c7"
    />
    <img width="1505" height="934" alt="image"
    src="https://github.com/user-attachments/assets/e4808e83-4095-484e-ae42-0e93632a0b8c"
    />
    <img width="1700" height="863" alt="image"
    src="https://github.com/user-attachments/assets/ccdfd140-51d6-4b71-b6c9-50abdfc85d20"
    />

--- website/src/components/admin/compare-solvers/SolverSelection.tsx
+++ website/src/components/admin/compare-solvers/SolverSelection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import ChartCompare from "./ChartCompare";
 import { IResultState } from "@/types/state";
