feea65913d3286a5d8c2eb70334a3a44c96976e5
Enrico Antonini <50218270+eantonini@users.noreply.github.com>
2026-08-03 14:23:29 +0200

    Removed paragraph at the end of key insignts page

--- website/src/components/key-insights/BenchmarkModelCases.tsx
+++ website/src/components/key-insights/BenchmarkModelCases.tsx
@@ -20,54 +20,6 @@ const BenchmarkModelCases = () => {
         solvers on, but is also a useful warning of the gaps in our collection.
       </p>
       <BenchmarkModelCasesTable />
-      <p>
-        {" "}
-        For version 2 of our platform, we plan to have a public call for
-        problems to address the gaps above. In particular, we welcome benchmark
-        problem contributions that cover:
-      </p>
-      <ul className="list-disc pl-6 space-y-2">
-        <li>
-          “Application”: DC optimal power flow, Operational and Production cost
-          modelling analyses for most modelling frameworks.
-        </li>
-        <li>
-          “Time horizon”: Multi Period analyses; something particularly
-          important as problems with multiple time horizons are more challenging
-          to solve.
-        </li>
-        <li>
-          “MILP features”: Unit commitment for TIMES and TEMOA (though, indeed,
-          they do not focus particularly on power sector modelling); other MILP
-          features, such as Piecewise fuel usage, Transmission switching and
-          Modularity are missing for most frameworks.
-        </li>
-        <li>
-          “Realistic”: Realistic problems are missing for PowerModels and Sienna
-        </li>
-        <li>
-          Large benchmark problems are also missing for many modelling
-          frameworks, see the section{" "}
-          <a
-            className="font-bold"
-            href="#what-is-feasible-for-open-source-solvers"
-          >
-            What is feasible for open solvers
-          </a>{" "}
-          above.
-        </li>
-      </ul>
-      <p>
-        <Link
-          className="font-bold"
-          href="/#contact"
-          aria-label="navigate to contact section"
-        >
-          Reach out
-        </Link>{" "}
-        to us if you&apos;d like to contribute any benchmark problems that can
-        fill the above gaps!
-      </p>
     </div>
   );
 };
